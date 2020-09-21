0d0c0b0770f068c92ceca797bed40866d904a3cb
AAE1990 <anton.akhmadeev@yandex.ru>
2020-09-21 14:39:52 +0500

    Updates

---
+++ precache-manifest.6dcdcec967f67c3dcc80d07eec1682d9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03904d3306839b2b21c86dd08f9a2fb",
+    "url": "/rect_with_bootstrap/index.html"
+  },
+  {
+    "revision": "1930c626f33e22561c08",
+    "url": "/rect_with_bootstrap/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "ec42370aa62b3700dfe5",
+    "url": "/rect_with_bootstrap/static/css/main.9fb3fce2.chunk.css"
+  },
+  {
+    "revision": "1930c626f33e22561c08",
+    "url": "/rect_with_bootstrap/static/js/2.d93deffd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rect_with_bootstrap/static/js/2.d93deffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec42370aa62b3700dfe5",
+    "url": "/rect_with_bootstrap/static/js/main.085b9e8e.chunk.js"
+  },
+  {
+    "revision": "3ff1916432e4cf5fa72b",
+    "url": "/rect_with_bootstrap/static/js/runtime-main.7e63c6a8.js"
+  },
+  {
+    "revision": "5ec12cd5360c5c331e46a82768a07c70",
+    "url": "/rect_with_bootstrap/static/media/high_tech.5ec12cd5.jpg"
+  },
+  {
+    "revision": "f6f4768e89c9e89546de6011eec172e1",
+    "url": "/rect_with_bootstrap/static/media/high_tech_red.f6f4768e.jpg"
+  },
+  {
+    "revision": "10306d3bbe21f74708aee7ff13a39abe",
+    "url": "/rect_with_bootstrap/static/media/image_dev.10306d3b.png"
+  },
+  {
+    "revision": "7c0b70e22f2054c9997bc0fa3658d086",
+    "url": "/rect_with_bootstrap/static/media/image_frameworks.7c0b70e2.jpg"
+  },
+  {
+    "revision": "f3b77fe60695d24141921239c1d8978e",
+    "url": "/rect_with_bootstrap/static/media/image_languages.f3b77fe6.jpeg"
+  },
+  {
+    "revision": "ede5b1c0b697303931ccee2fe2759ea0",
+    "url": "/rect_with_bootstrap/static/media/image_libraries.ede5b1c0.png"
+  },
+  {
+    "revision": "7d9b607dcfae9db2d0eae666344fa012",
+    "url": "/rect_with_bootstrap/static/media/image_team.7d9b607d.jpg"
+  }
+]);
